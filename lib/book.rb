e8b8d1e938dd335fa50f966c9fa42afb7fcccb6a
mercylynn <mercy.kamene@student.moringaschool.com>
2022-10-23 08:53:13 +0300

    Done

--- lib/book.rb
+++ lib/book.rb
@@ -1,2 +1,39 @@
+class Book
+    attr_reader :title
+    attr_accessor :author,:page_count,:genre,:turn_page
 
+    def initialize(title)
+        @title = title  
+    end
+    
+    # def author=(author)
+    #     @author = author
+    # end
 
+    # def page_count=(page_count)
+    #     @page_count = page_count
+    # end
+
+    # def genre=(genre)
+    #     @genre = genre
+    # end
+
+    def turn_page
+        puts "Flipping the page...wow, you read fast!"
+        
+    end
+
+  
+end
+
+book1 = Book.new("And Then There Were None")
+puts book1.title
+book1.author = "Agatha Christie"
+puts book1.author
+book1.page_count = 272
+puts book1.page_count
+book1.genre = "Mystery"
+puts book1.genre
+book2 = Book.new("The World According to Garp")
+# book2.turn_page = "Flipping the page...wow, you read fast!"
+puts book2.turn_page
--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,1 +1,46 @@
 # Make your shoe class here!
+
+class Shoe
+attr_reader :brand
+attr_accessor :color,:size, :condition, :material
+
+
+    def initialize(brand)
+        @brand = brand
+    end
+
+    # def color=(color)
+    #     @color = color
+    # end
+
+    # def size=(size)
+    #     @size = size
+    # end
+
+    # def condition=(condition)
+    #     @condition = condition
+    # end
+
+    # def material=(material)
+    #     @material = material
+    # end
+
+    def cobble
+        puts "Your shoe is as good as new!"
+        @condition = "new"
+      
+    end
+        
+end
+
+shoe1 = Shoe.new("Adidas")
+puts shoe1.brand
+shoe1.color = "red"
+puts shoe1.color
+shoe1.size = 9.5
+puts shoe1.size
+shoe1.condition = "old"
+puts shoe1.condition
+shoe1.material = "suede"
+puts shoe1.material
+puts shoe1.cobble
